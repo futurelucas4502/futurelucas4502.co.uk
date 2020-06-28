a44ebb0c3a59a7b0ae2a3efec6d96b8a3b93fdc3
Lucas Wilson <lucaswilson4502@outlook.com>
2020-06-28 14:31:42 +0100

    fixed up some styling

--- contribute.php
+++ contribute.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<?php include 'head.php';?>
-<link rel="preload" href="/images/contribute_background.png" as="image">
+  <?php include 'head.php'; ?>
+  <link rel="preload" href="/images/contribute_background.png" as="image">
 </head>
+
 <body>
-<?php include 'header.php';?>
-<div class="cover-image"></div>
-<div style="z-index:1" class="main-text">
-    <div class="col-md-12 text-center">
-        <h1 style="margin-top:5rem">Welcome to Contributing!</h1>
+  <?php include 'header.php'; ?>
+  <div class="cover-image">
+    <div class="main-text">
+      <div class="col-md-12 text-center">
+        <h1>Welcome to Contributing!</h1>
         <h3>I <i class="fas fa-heart" style="color: #f06595;margin-top:5rem"></i> Open-Source and you should too!</h3>
         <h5>Wondering why?</h5>
+      </div>
     </div>
   </div>
-<main class="text-center">
+  <main class="text-center">
 
-<p>Well it's because Open-Source allows for other programmers, developers and code enthusiasts to learn and contirbute to improve security, performance, add new features and suggest better practices.</p>
-</main>
-<?php include "footer.php"?>
+    <p>Well it's because Open-Source allows for other programmers, developers and code enthusiasts to learn and contirbute to improve security, performance, add new features and suggest better practices.</p>
+  </main>
+  <?php include "footer.php" ?>
 </body>
+
 </html>
